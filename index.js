90544cc47d19240e640ac96165704b43d2f68e61
v-eeru <133241025+v-eeru@users.noreply.github.com>
2023-05-11 23:12:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let display=document.querySelector('.display');
+let buttons=Array.from(document.querySelectorAll('.button'));
+buttons.map(button=>{
+button.addEventListener('click',e=>{
+   switch(e.target.innerText)
+   {
+    case 'C':
+        display.innerText='';
+        break;
+    case  '←':
+        if(display.innerText)
+        {
+        display.innerText= display.innerText.slice(0,-1);}
+        break;
+    case '=':
+        try{
+        display.innerText=eval(display.innerText);}
+        catch{
+            display.innerText="Error!";
+        }
+        break;
+       
+    default:
+        display.innerText+=e.target.innerText;
+        break;
+   }
+})
+})
